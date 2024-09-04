c4b0d465f18139d95eeb7c2bbd2424da30e5f8ef
Dev Ttangkong <ttankkeo112@gmail.com>
2024-09-04 11:36:52 +0900

    Added package image-encode-loader

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -49,10 +49,17 @@ module.exports = {
             // To export the image assets files from `src/` to `dist/`.
             // And by default, extensions are typical extensions supported by Chrome.
             {
-                test: /\.(png|jpe?g|webp|avif|hei[cf]|gif|bmp|tiff|ico)$/i,
+                test: /\.(png|jpe?g|webp|avif|hei[cf]|gif|tiff)$/i,
                 type: "asset/resource",
+                use: [{
+                    loader: "image-encode-loader",
+                    options: {
+                        // This format has the best compression rate at the moment.
+                        format: "avif"
+                    }
+                }],
                 generator: {
-                    filename: "images/[name][ext]"
+                    filename: "images/[name].avif"
                 }
             }
         ]
