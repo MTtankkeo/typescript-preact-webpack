7b5e52617f495e3bda5109b26518abc0084dd955
Dev Ttangkong <ttankkeo112@gmail.com>
2025-01-10 02:52:57 +0900

    Update svg.d.ts to adding image imports type

--- src/types/svg.d.ts
+++ src/types/svg.d.ts
@@ -4,3 +4,27 @@ declare module '*.svg' {
     const value: preact.FunctionComponent<preact.JSX.SVGAttributes>;
     export default value;
 }
+
+// Signature for the avif image imports that is representing the file's output path.
+declare module '*.avif' {
+    const value: string;
+    export default value;
+}
+
+// Signature for the webp image imports that is representing the file's output path.
+declare module '*.webp' {
+    const value: string;
+    export default value;
+}
+
+// Signature for the jpg image imports that is representing the file's output path.
+declare module '*.jpg' {
+    const value: string;
+    export default value;
+}
+
+// Signature for the png image imports that is representing the file's output path.
+declare module '*.png' {
+    const value: string;
+    export default value;
+}
